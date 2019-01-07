69fb4360ddf776af08e15cb4720b5075cd609141
Soulisack SAYYALINH <soulisacksayyalinh@Soulisacks-iMac.local>
2019-01-07 23:36:21 +0700

    updated

--- src/app/layouts/navbar/navbar.component.ts
+++ src/app/layouts/navbar/navbar.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { AuthServiceService } from 'src/app/cores/auth-service.service';
 import { DataServiceService } from 'src/app/cores/data-service.service';
 import { Router } from '@angular/router';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { FirstLoginComponent } from 'src/app/dialogs/first-login/first-login.component';
 
 @Component({
   selector: 'app-navbar',
@@ -10,7 +12,7 @@ import { Router } from '@angular/router';
 })
 export class NavbarComponent implements OnInit {
   authData: any;
-  constructor(private auth: AuthServiceService, private dataService: DataServiceService, private route: Router, ) {
+  constructor(private auth: AuthServiceService, private dataService: DataServiceService, private route: Router, private modalService: NgbModal) {
     let token = localStorage.getItem('abcd');
     if (!token) { route.navigateByUrl('/') }
     this.auth.tokenVerify(token).then(authData => {
@@ -47,5 +49,16 @@ export class NavbarComponent implements OnInit {
   loadMenubyUserId() {
     this.menus_all = this.dataService.getMenuByUserId(this.authData['id']);
   }
-
+  async updatePassword() {
+    const modalRef = this.modalService.open(FirstLoginComponent,
+      {
+        centered: true,
+      });
+    modalRef.componentInstance.data = this.authData;
+    modalRef.result.then(res => {
+      if (res == 'success') {
+        alert('Password updated');
+      }
+    });
+  }
 }
