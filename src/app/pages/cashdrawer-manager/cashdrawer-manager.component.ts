85020d828adda79d911001a8538059df6de95621
Soulisack SAYYALINH <soulisacksayyalinh@Soulisacks-iMac.local>
2019-01-10 23:55:16 +0700

    update user cash load

--- src/app/pages/cashdrawer-manager/cashdrawer-manager.component.ts
+++ src/app/pages/cashdrawer-manager/cashdrawer-manager.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-cashdrawer-manager',
@@ -8,8 +9,24 @@ import { Component, OnInit } from '@angular/core';
 export class CashdrawerManagerComponent implements OnInit {
 
   constructor() { }
-
+  cashloadForm: FormGroup;
+  btnDisabled = false;
+  showBox = 'hidden';
   ngOnInit() {
-  }
+    this.cashloadForm = new FormGroup({
 
+    });
+    this.btnDisabled = false;
+  }
+  loadCashAmountChange(value) {
+    if (value && value >= 100000) {
+      this.btnDisabled = true;
+    } else {
+      this.btnDisabled = false;
+    }
+  }
+  processLoadCash() {
+    this.btnDisabled = true;
+    this.showBox = '';
+  }
 }
